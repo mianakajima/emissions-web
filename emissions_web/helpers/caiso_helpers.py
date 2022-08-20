907b02caa523e30ca3b69db047a2046a06d0b631
mianakajima <mb.nakajima@gmail.com>
2022-08-19 18:02:20 -0700

    Helper function to get grid forecast and start data cleaning

--- emissions_web/helpers/caiso_helpers.py
+++ emissions_web/helpers/caiso_helpers.py
@@ -1,7 +1,13 @@
 from datetime import date
+
+import pandas
 import requests
 from io import StringIO
 import pandas as pd
+import zipfile
+import xml.etree.ElementTree as ET
+import re
+import os
 
 def get_supply_trend_today():
     """Get today's supply trend from CAISO website. """
@@ -32,3 +38,58 @@ def get_percentage_renewable(df):
     df_new['RenewablePercentage'] = 100*df_new['Renewables']/df_new['Total']
 
     return df_new
+
+def get_oasis_forecast(start_date, end_date, market_run_id, data_item):
+    """Returns OASIS API call as pandas dataframe
+    For data_item:
+    - 'ENE_SLRS': Generation
+    - 'SLD_REN_FCST': Solar and Wind Forecast sol
+
+    """
+
+    url = f"http://oasis.caiso.com/oasisapi/SingleZip?queryname={data_item}&" \
+                    f"startdatetime={start_date}T07:00-0000&" \
+                    f"enddatetime={end_date}T07:00-0000&version=1&" \
+                    f"market_run_id={market_run_id}"
+    # Make request and unzip file
+    r = requests.get(url)
+
+    # extract zipfile
+    with open('./temp_zip.zip', 'wb') as code:
+        code.write(r.content)
+
+    with zipfile.ZipFile('./temp_zip.zip', 'r') as zip_ref:
+        zip_ref.extractall('./download_data')
+
+    # get name of XML file
+    header_filename = r.headers['Content-Disposition']
+    zip_file = re.sub('inline; filename=', '', header_filename)
+    xml_file = re.sub('.zip;', '.xml', zip_file)
+    os.remove('temp_zip.zip')
+    # parse XML file
+
+    xml_data = ET.parse(f"download_data/{xml_file}")
+    root = xml_data.getroot()
+
+    # set of unique tags in file
+    tags = {elem.tag for elem in root.iter()}
+
+    data_to_collect = {'value': 'VALUE',
+                       'start_date': 'INTERVAL_START_GMT',
+                       'end_date': 'INTERVAL_END_GMT',
+                       'trading_hub': 'TRADING_HUB',
+                       'ren_type': 'RENEWABLE_TYPE',
+                       'resource_name': 'RESOURCE_NAME',
+                       'data_item': 'DATA_ITEM'}
+
+    data_collected = {}
+
+    for value in data_to_collect:
+        tag = f'{{http://www.caiso.com/soa/OASISReport_v1.xsd}}{data_to_collect[value]}'
+        if tag in tags:
+            data_collected[value] = [repdata.text for repdata in root.iter(tag)]
+
+    df = pandas.DataFrame(data_collected)
+
+
+    return df
